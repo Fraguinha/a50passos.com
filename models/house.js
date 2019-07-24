e86f350a797e47d1795cfdbaaee84f0521621bd5
João Fraga <joaoluisfreirefraga@gmail.com>
2019-07-24 22:46:05 +0100

    Changed house id from number to string

--- models/house.js
+++ models/house.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const house_schema = new mongoose.Schema({
     id: {
-        type: Number,
+        type: String,
         required: true
     },
     available: {

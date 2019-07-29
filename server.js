62c02152b91c9cdf51edcb923516c83f7647770e
João Fraga <joaoluisfreirefraga@gmail.com>
2019-07-29 14:10:21 +0100

    Added eslint

--- server.js
+++ server.js
@@ -1,51 +1,51 @@
 // Requires
-const express = require('express')
-const bodyParser = require('body-parser')
-const dotenv = require('dotenv')
-const fs = require('fs')
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const fs = require("fs");
 
 // Dotenv config
-dotenv.config()
+dotenv.config();
 
 // Modules
-const passport = require('./modules/setup/passport')
-const database = require('./modules/setup/database')
-const viewengine = require('./modules/setup/viewengine')
+const passport = require("./modules/setup/passport");
+const database = require("./modules/setup/database");
+const viewengine = require("./modules/setup/viewengine");
 
 // Application
-const app = express()
+const app = express();
 
 // Express bodyParser
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Passport config
-passport.configure(app)
+passport.configure(app);
 
 // Filesystem
-if (!fs.existsSync('public/uploads/')) {
-  fs.mkdirSync('public/uploads/')
+if (!fs.existsSync("public/uploads/")) {
+  fs.mkdirSync("public/uploads/");
 }
 
 // Database
-database.startup()
+database.startup();
 
 // Views
-viewengine.configure(app)
+viewengine.configure(app);
 
 // Routers
-app.use('/', require('./routes/root'))
+app.use("/", require("./routes/root"));
 
-app.use('/catalog', require('./routes/catalog'))
-app.use('/house', require('./routes/house'))
-app.use('/login', require('./routes/login'))
-app.use('/dashboard', require('./routes/dashboard'))
+app.use("/catalog", require("./routes/catalog"));
+app.use("/house", require("./routes/house"));
+app.use("/login", require("./routes/login"));
+app.use("/dashboard", require("./routes/dashboard"));
 
-app.get('/*', (req, res) => {
-  res.write('Page not found.')
-  res.status(404).end()
-})
+app.get("/*", (req, res) => {
+  res.write("Page not found.");
+  res.status(404).end();
+});
 
 // Start server
-const port = process.env.PORT || 8080
-app.listen(port, console.log(`Started listening on port ${port}`))
+const port = process.env.PORT || 8080;
+app.listen(port, console.log(`Started listening on port ${port}`));

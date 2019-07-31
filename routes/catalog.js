88a287da80c5253ce673ffab04ba62c84498e004
João Fraga <joaoluisfreirefraga@gmail.com>
2019-07-31 05:41:50 +0100

    Added pagination

--- routes/catalog.js
+++ routes/catalog.js
@@ -3,39 +3,63 @@ const router = express.Router();
 const House = require("../models/house");
 
 // Show catalog
-router.get("/", async (req, res) => {
+router.get("/:page", async (req, res) => {
   try {
-    const houses = await House.find();
+    const number = 9;
+    const page = req.params.page;
+    const houses = await House.find()
+      .skip(number * page)
+      .limit(number);
+    const count = await House.countDocuments();
     res.render("main/catalog.ejs", {
-      data: houses
+      data: houses,
+      current: page,
+      pages: Math.ceil(count / number),
+      url: "/catalog"
     });
   } catch (err) {
     res.redirect("/");
   }
 });
 
-router.get("/rooms", async (req, res) => {
+router.get("/rooms/:page", async (req, res) => {
   try {
+    const number = 9;
+    const page = req.params.page;
     const houses = await House.find(
       { type: 1 },
       "id title address description tip wc available suite elevator dinningroom balcony gardin photo1 photo2 photo3 photo4"
-    );
+    )
+      .skip(number * page)
+      .limit(number);
+    const count = await House.countDocuments({ type: 1 });
     res.render("main/catalog.ejs", {
-      data: houses
+      data: houses,
+      current: page,
+      pages: Math.ceil(count / number),
+      url: "/catalog/rooms"
     });
   } catch (err) {
     res.redirect("/");
   }
 });
 
-router.get("/apartments", async (req, res) => {
+router.get("/apartments/:page", async (req, res) => {
   try {
+    const number = 9;
+    const page = req.params.page;
     const houses = await House.find(
       { type: 2 },
       "id title address description tip wc available suite elevator dinningroom balcony gardin photo1 photo2 photo3 photo4"
-    );
+    )
+      .skip(number * page)
+      .limit(number);
+    const count = await House.countDocuments({ type: 2 });
     res.render("main/catalog.ejs", {
-      data: houses
+      data: houses,
+      current: page,
+      pages: Math.ceil(count / number),
+      url: "/catalog/apartments"
     });
   } catch (err) {
     res.redirect("/");

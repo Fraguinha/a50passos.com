64a986810c58c61571c474278fe73ce4ecadaea5
Fraguinha <joaoluisfreirefraga@gmail.com>
2020-09-23 23:48:01 +0100

    Improve pagination

--- routes/catalog.js
+++ routes/catalog.js
@@ -5,17 +5,19 @@ const House = require("../models/house-model");
 // Show catalog
 router.get("/:page", async (req, res) => {
   try {
-    const number = 9;
-    const page = req.params.page;
+    const numberItems = 9;
+    const paginationNumber = 5;
+    const page = Number(req.params.page);
     const houses = await House.find()
       .sort({ date: -1 })
-      .skip(number * page)
-      .limit(number);
+      .skip(numberItems * page)
+      .limit(numberItems);
     const count = await House.countDocuments();
     res.render("main/catalog.ejs", {
       data: houses,
       current: page,
-      pages: Math.ceil(count / number),
+      pagination: paginationNumber,
+      pages: Math.ceil(count / numberItems),
       url: "/catalog"
     });
   } catch (err) {
@@ -25,17 +27,19 @@ router.get("/:page", async (req, res) => {
 
 router.get("/rooms/:page", async (req, res) => {
   try {
-    const number = 9;
-    const page = req.params.page;
+    const numberItems = 9;
+    const paginationNumber = 5;
+    const page = Number(req.params.page);
     const houses = await House.find({ type: 1 })
       .sort({ date: -1 })
-      .skip(number * page)
-      .limit(number);
+      .skip(numberItems * page)
+      .limit(numberItems);
     const count = await House.countDocuments({ type: 1 });
     res.render("main/catalog.ejs", {
       data: houses,
       current: page,
-      pages: Math.ceil(count / number),
+      pagination: paginationNumber,
+      pages: Math.ceil(count / numberItems),
       url: "/catalog/rooms"
     });
   } catch (err) {
@@ -45,17 +49,19 @@ router.get("/rooms/:page", async (req, res) => {
 
 router.get("/apartments/:page", async (req, res) => {
   try {
-    const number = 9;
-    const page = req.params.page;
+    const numberItems = 9;
+    const paginationNumber = 5;
+    const page = Number(req.params.page);
     const houses = await House.find({ type: 2 })
       .sort({ date: -1 })
-      .skip(number * page)
-      .limit(number);
+      .skip(numberItems * page)
+      .limit(numberItems);
     const count = await House.countDocuments({ type: 2 });
     res.render("main/catalog.ejs", {
       data: houses,
       current: page,
-      pages: Math.ceil(count / number),
+      pagination: paginationNumber,
+      pages: Math.ceil(count / numberItems),
       url: "/catalog/apartments"
     });
   } catch (err) {

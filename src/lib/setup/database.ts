53f9b98b4dfa1ab01ef818368cd577d7995d0b24
Fraguinha <joaoluisfreirefraga@gmail.com>
2021-08-27 11:46:53 +0100

    Update database

--- src/lib/setup/database.ts
+++ src/lib/setup/database.ts
@@ -1,37 +1,14 @@
 // Requires
-import bcrypt from 'bcryptjs'
 import mongoose from 'mongoose'
-import User from '../../models/user-model'
 
 // Functions
 const start = (database: string) => {
-  mongoose
-    .connect(database, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    })
-    .catch((_) => {
-      process.exit(1)
-    })
+  mongoose.connect(database).catch((_) => {
+    process.exit(1)
+  })
   const db = mongoose.connection
   db.on('open', () => {
     console.log('Connected to database')
-    User.find((_, res) => {
-      if (res.length === 0) {
-        const defaultAdmin = new User({
-          email: 'admin@admin',
-          password: 'admin',
-        })
-        bcrypt.genSalt(10, (_, salt) => {
-          bcrypt.hash(defaultAdmin.password, salt, (_, hash) => {
-            defaultAdmin.password = hash
-            defaultAdmin.save()
-          })
-        })
-      }
-    })
   })
 }
 

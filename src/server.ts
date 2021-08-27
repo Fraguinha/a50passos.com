ba91f99b1bcd78e3551d88983929afd18be6f7ff
Fraguinha <joaoluisfreirefraga@gmail.com>
2021-08-27 12:00:13 +0100

    Remove deprecated package

--- src/server.ts
+++ src/server.ts
@@ -1,42 +1,42 @@
 // Imports
-import dotenv from 'dotenv';
-import express from 'express';
-import bodyparser from './lib/setup/bodyparser';
-import database from './lib/setup/database';
-import filesystem from './lib/setup/filesystem';
-import passport from './lib/setup/passport';
-import routes from './lib/setup/routes';
-import viewengine from './lib/setup/viewengine';
+import dotenv from 'dotenv'
+import express from 'express'
+import database from './lib/setup/database'
+import filesystem from './lib/setup/filesystem'
+import parsing from './lib/setup/parsing'
+import passport from './lib/setup/passport'
+import routes from './lib/setup/routes'
+import viewengine from './lib/setup/viewengine'
 
 // Dotenv config
-dotenv.config();
+dotenv.config()
 
 // Environment variables
-const PORT = process.env.PORT || 3000;
-const APPNAME = process.env.APPNAME || 'app';
-const SECRET = process.env.SESSION_SECRET || 'secret';
-const DATABASE = process.env.DATABASE || `mongodb://localhost/${APPNAME}`;
+const PORT = process.env.PORT || 3000
+const APPNAME = process.env.APPNAME || 'app'
+const SECRET = process.env.SESSION_SECRET || 'secret'
+const DATABASE = process.env.DATABASE || `mongodb://localhost/${APPNAME}`
 
 // Application
-const app = express();
+const app = express()
 
 // Database
-database.start(DATABASE);
+database.start(DATABASE)
 
 // Filesystem
-filesystem.configure();
+filesystem.configure()
 
 // BodyParser config
-bodyparser.configure(app);
+parsing.configure(app)
 
 // Passport config
-passport.configure(app, SECRET);
+passport.configure(app, SECRET)
 
 // Views
-viewengine.configure(app);
+viewengine.configure(app)
 
 // Routers
-routes.configure(app);
+routes.configure(app)
 
 // Start server
-app.listen(PORT, () => console.log(`Started listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Started listening on port ${PORT}`))

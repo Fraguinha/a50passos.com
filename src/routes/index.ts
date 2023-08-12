060684b01ee4775ab749e8767f3c93cacd37f95a
João Fraga <joao@fraguinha.com>
2023-08-12 20:58:26 +0100

    Improve SEO

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,7 +7,7 @@ const router = express.Router()
 router.get('/', async (req, res) => {
   const meta = await Meta.findOne().exec()
   res.render('index.ejs', {
-    title: 'Quartos e Apartamentos para Arrendar no Pólo Universitário do Porto',
+    title: 'Quartos e Apartamentos para Arrendar no Porto',
     description:
       'És estudante? Estás à procura de arrendar no Porto? Seja qual for a Universidade temos um espaço a 50 passos!',
     authenticated: req.isAuthenticated(),
